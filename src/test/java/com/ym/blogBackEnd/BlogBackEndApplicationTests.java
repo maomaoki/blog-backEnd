8f35a8c5e7791c0dc84ec6eaf1307017cb5689c1
猫猫 <546211257@qq.com>
2025-01-24 22:19:22 +0800

    first commit

---
+++ src/test/java/com/ym/blogBackEnd/BlogBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ym.blogBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
